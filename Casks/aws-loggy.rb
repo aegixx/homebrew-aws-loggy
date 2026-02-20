91d02b20240fc65c5bf5dc00e05fc65d779b5c9d
Bryan Stone <bryan@manifest.finance>
2026-02-20 13:47:49 -0600

    feat: add aws-loggy cask v3.7.1

---
+++ Casks/aws-loggy.rb
@@ -0,0 +1,24 @@
+cask "aws-loggy" do
+  version "3.7.1"
+
+  on_arm do
+    url "https://github.com/aegixx/aws-loggy/releases/download/v#{version}/Loggy-#{version}-macos-arm64.dmg"
+    sha256 "e8e1f3404b6b433f5ca94330b77b2afc9b1adf98e2628e0b4fba0729b81e45f7"
+  end
+  on_intel do
+    url "https://github.com/aegixx/aws-loggy/releases/download/v#{version}/Loggy-#{version}-macos-x86_64.dmg"
+    sha256 "24341603f40adab310740acb75f1bb0afc453e8d94b20b516037f7ea5a4a07e2"
+  end
+
+  name "Loggy"
+  desc "A fast, native desktop app for browsing and tailing AWS CloudWatch logs"
+  homepage "https://github.com/aegixx/aws-loggy"
+
+  app "Loggy.app"
+
+  zap trash: [
+    "~/Library/Application Support/dev.steampunk.aws-loggy",
+    "~/Library/Caches/dev.steampunk.aws-loggy",
+    "~/Library/Preferences/dev.steampunk.aws-loggy.plist",
+  ]
+end
